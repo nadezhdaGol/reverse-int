bd223f70fe6b8cde1be03fa1c493ab242c67721c
nadezhdaGol <78170166+nadezhdaGol@users.noreply.github.com>
2021-02-20 00:59:32 +0300

    fix: correct solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-    return String(n).split('').reverse().join('');  
+    if (n % 10 === 0 && n < 10) {
+        let mp = n / 10 * (-1);
+        return String(mp).split('').reverse().join('');
+    } else if (n % 10 === 0) {
+        let m = n / 10;
+        return String(m).split('').reverse().join('');
+    } else if (n < 0) {
+        let p = n * (-1);
+        return String(p).split('').reverse().join('');
+    } else {
+        return String(n).split('').reverse().join('');
+    }
 }
